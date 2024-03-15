1c257b117576c88b50056b3c0e3d7323c9c840ac
ivan <ivan-guita@hotmail.com>
2024-03-15 10:47:41 -0300

    rota para gravar a categoria funcionando final da aula 81

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const { default: slugify } = require("slugify");
 const router = express.Router();
+const Category = require("./Category");
 
 
-
+//rota para o formulario
 router.get("/admin/categories/new",(req,res)=>{
 
     res.render("admin/categories/new");
     
 });
+
+
+//rota de salvar
+router.post("/categories/save",(req,res)=>{
+
+    var title = req.body.title;
+
+    if(title != undefined){
+      
+        Category.create({
+
+            title: title,
+            slug: slugify(title)
+
+        }).then(()=>{
+
+            res.redirect("/");
+        });
+
+
+    }else{
+
+        res.redirect("/admin/categories/new")
+    }
+
+})
 
 module.exports = router;
