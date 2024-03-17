74192e0c3d9011da0a21544160aad4558602c42e
ivan <ivan-guita@hotmail.com>
2024-03-16 22:42:32 -0300

    base do formulario de criação de artigos/rota prontos fim da aula 90

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/admin/articles/new",(req,res)=>{
-    res.send("Rota pra criar novo artigo")
+    res.render("admin/articles/new");
 });
 
 router.get("/admin/articles",(req,res)=>{
