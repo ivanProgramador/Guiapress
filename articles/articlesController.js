f24e60e1de3952db9395f8c68a1ed96c72409b31
ivan <ivan-guita@hotmail.com>
2024-03-18 01:23:33 -0300

    função pra criar artigo pronta final da aula 95

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const { default: slugify } = require("slugify");
+
 
 router.get("/admin/articles/new",(req,res)=>{
    Category.findAll().then(categories=>{
@@ -15,5 +17,22 @@ router.get("/admin/articles/new",(req,res)=>{
 router.get("/admin/articles",(req,res)=>{
     res.send("Rota de artigos")
 });
+
+router.post("/articles/save",(req,res)=>{
+
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title:title,
+        slug:slugify(title),
+        body: body,
+        categoryId: category
+    })
+
+    
+
+})
 
 module.exports = router;
