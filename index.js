923127b777122f1af6197ec70f4705c6463df2bf
ivan <ivan-guita@hotmail.com>
2024-03-19 00:17:36 -0300

    navbar home criada, a pagina inicial ja mostra os artigos fim da aula 101

--- index.js
+++ index.js
@@ -34,7 +34,13 @@ app.use("/",articlesController);
 
 app.get("/",(req,res)=>{
 
-    res.render("index");
+    Article.findAll().then(articles=>{
+
+        res.render("index",{articles:articles});
+
+    })
+
+  
 
 });
 
