bc84f47c71bde20494f2f4cc05dfa6c18b9247a8
ivan <ivan-guita@hotmail.com>
2024-03-19 00:49:33 -0300

    foi feito o join pra mostrar o titulo da categoria na tabela de artigos fim da aula 99

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ app.use("/",articlesController);
 
 app.get("/",(req,res)=>{
 
-    Article.findAll().then(articles=>{
+    Article.findAll({
+        order:[['id','DESC']]
+    }).then(articles=>{
         res.render("index",{articles:articles});
     })
 });
