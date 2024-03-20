ef5f836776ce8857c07a2dc84310f2ad6ca1c886
ivan <ivan-guita@hotmail.com>
2024-03-20 01:03:23 -0300

    link estático de paginação adicionado a pagina principal fim da aula 113

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -161,7 +161,8 @@ router.get("/articles/page/:num",(req,res)=>{
     Article.findAndCountAll({
 
         limit:4,
-        offset: offset
+        offset: offset,
+        order:[['id','DESC']]
 
     }).then(articles=>{
     
