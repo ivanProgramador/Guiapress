ccbd7fd838c85d3159616a3e217004301e4afdff
ivan <ivan-guita@hotmail.com>
2024-03-20 20:44:26 -0300

    logica pra evitar cadastro de email duplicado concluida final da aula 120

--- user/usersController.js
+++ user/usersController.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 
 //rotas de formulario
 
-router.get("/admin/user/new",(req,res)=>{
+router.get("/admin/users/new",(req,res)=>{
     res.render("admin/users/new");
 });
 
@@ -32,29 +32,47 @@ router.get("/admin/user/edit/:id",(req,res)=>{
 
 //rotas de execução 
 
+
+
+
 //adicionar usuario
 
 router.post("/user/save",(req,res)=>{
     
     var email = req.body.email;
     var password = req.body.password;
-   
-    //usando o salt pra gerar uma hash auxiliar
-    var salt = bcrypt.genSaltSync(10);
-    // passado apra variavel hash a senha conevrtida mais o salt
-    var hash = bcrypt.hashSync(password,salt);
-    
-    
-    User.create({
-        email:email,
-        password:hash
-    }).then(()=>{
-        res.redirect("/admin/users");
-    }).catch(err =>{
-        res.redirect("/admin/users");
 
+    //um usuario não pode ter o mesmo email do outro
+    //abaixo eu faço uma pesquisa se a avrivek gerada troxer uma email
+    //o cliente voltapra pagina de cadastro se não ele grava
+    
+    User.findOne({where:{email:email}}).then(user=>{
+        if(user == undefined){
+
+            //usando o salt pra gerar uma hash auxiliar
+            var salt = bcrypt.genSaltSync(10);
+            // passado apra variavel hash a senha conevrtida mais o salt
+            var hash = bcrypt.hashSync(password,salt);
+
+            User.create({
+                email:email,
+                password:hash
+            }).then(()=>{
+                res.redirect("/admin/users");
+            }).catch(err =>{
+                res.redirect("/admin/users");
+
+            })
+        }else{
+            res.redirect("/admin/users/new");
+        }
     })
+   
 });
+
+
+
+
 
 
 router.post("/user/delete",(req,res)=>{
@@ -64,6 +82,7 @@ router.post("/user/delete",(req,res)=>{
         res.redirect("/admin/users")
     })
 });
+
 
 router.post("/user/update",(req,res)=>{
     var id = req.body.id;
